37001bedac9ddb90c7e0a51d7d66f8001f724ca6
DouglasM02 <55521413+DouglasM02@users.noreply.github.com>
2022-03-20 23:40:17 -0300

    trocar return por die

--- src/services/findUser.php
+++ src/services/findUser.php
@@ -11,7 +11,7 @@
             return $data ?? false;
         }
         catch(PDOException $error) {
-            return $error->getMessage();
+            die( $error->getMessage());
         }
     }
 
