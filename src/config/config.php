97a97edcda02c0e537126129b23fe046f585397f
DouglasM02 <55521413+DouglasM02@users.noreply.github.com>
2022-09-07 22:29:57 -0300

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-const DB_DRIVE = 'mysql';
-const DB_HOST = 'localhost';
-const DB_DATABASE_NAME = "projeto";
-const DB_USER = "root";
+const DB_DRIVE = '';
+const DB_HOST = '';
+const DB_DATABASE_NAME = "";
+const DB_USER = "";
 const DB_PASSWORD = "";
 
 ?>
